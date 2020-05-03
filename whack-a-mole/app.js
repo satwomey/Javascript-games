b25300115a7d1641de6dc5b828fff18bc1ab13be
satwomey <stephanieatwomey@gmail.com>
2020-05-02 22:38:41 -0400

    Finished up whack a mole game.

---
+++ whack-a-mole/app.js
@@ -0,0 +1,42 @@
+const square = document.querySelectorAll('.square')
+const mole = document.querySelectorAll('.mole')
+const timeLeft = document.querySelector('#time-left')
+let score = document.querySelector('#score')
+
+let result = 0
+let currentTime = timeLeft.textContent
+
+function randomSquare(){
+  square.forEach(className => {
+    className.classList.remove('mole')
+  })
+  let randomPosition = square[Math.floor(Math.random()*9)]
+  randomPosition.classList.add('mole')
+  hitPosition = randomPosition.id
+}
+
+square.forEach(id => {
+  id.addEventListener('mouseup',() =>{
+    if(id.id === hitPosition){
+      result += 1
+      score.textContent = result
+    }
+  })
+})
+
+function moveMole(){
+  let timerID = null
+  timerID = setInterval(randomSquare, 1000)
+}
+
+moveMole()
+
+function countDown(){
+  currentTime--
+  timeLeft.textContent = currentTime
+  if(currentTime === 0){
+    clearInterval(timerID)
+    alert("Game over! Final score: "+result)
+  }
+}
+let timerID = setInterval(countDown, 1000)
